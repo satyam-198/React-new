480aab229989d9d17f42e8d0cf8b276e5ad22ee3
satyam-198 <sk4225501@gmail.com>
2022-03-21 21:10:13 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter, Route, Switch} from 'react-router-dom';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
-
+import Home from './components/home.component';
+import Nav from './components/nav.component';
+import Login from "./components/login.component";
+import Register from './components/register.component';
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav/>
+      <div className="auth-wrapper">
+        <div className="auth-inner">
+          <Switch>
+            <Route exact path="/" component= {Home} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/register" component={Register} />
+          </Switch>
+        </div>
+      </div>
     </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/home.component.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+import React, {Component} from "react";
+
+export default class Home extends Component {
+
+    componentDidMount(){
+        //axios.get
+    }
+    render()
+    {
+        return(
+            <h2>You are not logged in!</h2>
+        )
+    }
+}
